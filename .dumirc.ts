0e607a94263efb5d72483050cddb7f2b219bcb03
826300474 <826300474@qq.com>
2024-05-29 15:39:34 +0800

    init

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base:'/czy-component/',
+  publicPath: '/czy-component/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'czy-component',
